6c644e9c05760c9534c7fc385568f94b5e9f140e
Madelen Yoo <rolemadelen@tuta.io>
2020-08-02 08:36:20 +0900

    timetable3 code refactoring still in progress & inputs width changed
    Signed-off-by: Jii Yoo <jiiyoo@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@tuta.io>

--- timetable3/vuecode.js
+++ timetable3/vuecode.js
@@ -20,54 +20,52 @@ const app = new Vue({
   methods: {
     tableFormSubmit(e) {
       e.preventDefault();
-      this.submitted = true;
-    },
-    newPeriod() {
       this.endTime = ('0000'+((1*this.startTime)+100)).toString().slice(-4);
 
       this.schedule.push({period: `[${this.schedule.length+1-this.longBreakCount}] 
                                     ${this.startTime.slice(0,2)}:${this.startTime.slice(2,4)} - 
                                     ${this.endTime.slice(0,2)}:${this.endTime.slice(2,4)}`, 
-        fin: false});
-
+                                    fin: false});
+      this.submitted = true;
+    },
+    newPeriod() {
       this.startTime = ('0000'+((1*this.endTime)+this.interval)).toString().slice(-4);
       let hour = 1*this.startTime.slice(0,2);
       let min = 1*this.startTime.slice(2,4);
 
       if(min>59) {
-        min = '00';
+        min -= 60;
         hour += 1
       }
       hour = ('00'+hour).slice(-2);
-      this.startTime = `${hour}${min}`
+      min = ('00'+min).slice(-2);
+      this.startTime = `${hour}${min}`;
 
-      this.endTime = ('0000'+((1*this.endTime)+100)).toString().slice(-4);
+      hour = 1*hour;
+      hour += 1
 
-      hour = 1*this.endTime.slice(0,2);
-      min = 1*this.endTime.slice(2,4);
-
-      if(min>59) {
-        min = '00';
-        hour += 1
-      }
       hour = ('00'+hour).slice(-2);
-      this.endTime = `${hour}${min}`
+      this.endTime = `${hour}${min}`;
+      this.schedule.push({period: `[${this.schedule.length+1-this.longBreakCount}] 
+                                    ${this.startTime.slice(0,2)}:${this.startTime.slice(2,4)} - 
+                                    ${this.endTime.slice(0,2)}:${this.endTime.slice(2,4)}`, 
+                                    fin: false});
     },
     newBreak() {
       this.schedule.push({period: 'Long break', fin: false})
 
-      let hour = 1*this.startTime.slice(0,2);
-      let min = 1*this.startTime.slice(2,4) + 45;
+      let hour = 1*this.endTime.slice(0,2);
+      let min = 1*this.endTime.slice(2,4) + 45;
 
-      if(min>59) {
-        hour +=1;
-        min = min-60;
+      if (min > 59) {
+        hour += 1
+        min -= 60;
       }
 
       hour = ('00'+hour).slice(-2);
       min = ('00'+min).slice(-2);
 
-      this.startTime = `${hour}${min}`;
+      this.endTime = `${hour}${min}`;
       this.longBreakCount += 1;
     },
     clearSchedule() {
